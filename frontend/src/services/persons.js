16908a2000fd86b830a7016846da4c1be6940243
Pierpaolo Giangravè <pierpyg73@gmail.com>
2025-01-20 10:32:49 +0100

    Update persons.js

--- frontend/src/services/persons.js
+++ frontend/src/services/persons.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const baseUrl = '${import.meta.env.VITE_BACKEND_URL}/api/persons'
+const baseUrl = `${import.meta.env.VITE_BACKEND_URL}/api/persons`
 
 const getAll =() => {
     return axios.get(baseUrl)
